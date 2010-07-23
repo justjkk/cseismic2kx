d00fb2313f1d964037e951e147c6668b14dc4420
Kishore kumar J <jkk.2k9@gmail.com>
2010-07-23 22:58:49 +0530

    Activation bug fixed

--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^$', 'home.views.index'),
-    url(r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate', { 'backend': 'registration.backends.default.DefaultBackend' }, name='registration_activate'),
+    url(r'^account/activate/(?P<activation_key>\w+)/$', 'registration.views.activate', { 'backend': 'registration.backends.default.DefaultBackend' }, name='registration_activate'),
     url(r'^account/signup/$', 'registration.views.register', {'backend':'registration.backends.default.DefaultBackend' }, name='registration_register'),
     (r'^account/', include('django_authopenid.urls')),
 )
