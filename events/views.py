944abe1226a8053ce5309432eb03bd92e979ef69
Kishore kumar J <jkk.2k9@gmail.com>
2010-08-02 21:20:59 +0530

    Made event timings optional - minor edit

--- events/views.py
+++ events/views.py
@@ -16,5 +16,8 @@ def schedule(request):
     for i in range(0,len(scheduled_events)):
         if scheduled_events[i][1] is None:
             scheduled_events[i] = scheduled_events[i][:1] + ('-',) + scheduled_events[i][2:]
+        if scheduled_events[i][2] is None:
+            scheduled_events[i] = scheduled_events[i][:2] + ('-',) + scheduled_events[i][3:]
+
     scheduled_events.sort(key=lambda x: str(x[1]))
     return render_to_response('events/schedule.html', {'events': scheduled_events}, context_instance=RequestContext(request), mimetype='text/html')
