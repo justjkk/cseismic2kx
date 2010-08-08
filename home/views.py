2113c1142fb14555c16d67da51d44797de069018
Kishore kumar J <jkk.2k9@gmail.com>
2010-08-08 11:46:20 +0530

    Updated sitemap

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from home.models import NewsItem
+from events.models import Event
 
 def index(request):
     news_items = NewsItem.objects.order_by('-date')[:5]
@@ -10,4 +11,5 @@ def gallery(request):
     return render_to_response('gallery.html', context_instance=RequestContext(request), mimetype='text/html')
 
 def sitemap(request):
-    return render_to_response('sitemap.html', context_instance=RequestContext(request), mimetype='text/html')
+    e = Event.objects.all()
+    return render_to_response('sitemap.html', {'events': e}, context_instance=RequestContext(request), mimetype='text/html')
