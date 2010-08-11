8b41a777afb54c8f060cb24fd0a9090c9bf79281
jkk <jkk.2k9@gmail.com>
2010-08-11 21:34:18 +0530

    Added participantInline for Events

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,13 @@
 from models import Event
 from django.contrib import admin  
-  
-admin.site.register(Event)
+from participantsprofile.models import Participant
+
+class ParticipantInline(admin.TabularInline):
+    model = Participant.events.through
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+                ParticipantInline,
+              ]
+admin.site.register(Event, EventAdmin)
 
