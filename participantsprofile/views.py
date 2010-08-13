ee03791ac7c705544ed0c709603748282792ca57
Kishore kumar J <jkk.2k9@gmail.com>
2010-08-14 00:16:16 +0530

    Added Event participation link to site

--- participantsprofile/views.py
+++ participantsprofile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
@@ -8,7 +8,10 @@ from forms import ParticipantEventsForm
 @login_required
 def participant_events(request):
     if request.method == 'POST':
-        participant = request.user.get_profile().participant_set.all()[0]
+        try:
+            participant = request.user.get_profile().participant_set.all()[0]
+        except Exception:
+            return HttpResponse('This page is not for you!<br><a href="/">Take me back Home</a>')
         form = ParticipantEventsForm(request.POST)
         participant.events = form.data.getlist('events')
         for e in form.data.getlist('events'):
@@ -16,7 +19,10 @@ def participant_events(request):
                 participant.events.add(e)
         return HttpResponseRedirect('/')
     else:
-        participant = request.user.get_profile().participant_set.all()[0]
+        try:
+            participant = request.user.get_profile().participant_set.all()[0]
+        except Exception:
+            return HttpResponse('This page is not for you!<br><a href="/">Take me back Home</a>')
         events = [ e.id for e in participant.events.all()]
         form = ParticipantEventsForm(data = {'events':events})
     return render_to_response('participantsprofile/participant_events.html', {'form':form}, context_instance=RequestContext(request), mimetype='text/html')
