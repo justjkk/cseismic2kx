b4da6bac4d6523be03d4c0ad097bb1697531baa0
Kishore kumar J <jkk.2k9@gmail.com>
2010-08-23 09:26:28 +0530

    Bugfix: Transport Text-only order of urlpatterns

--- transport/urls.py
+++ transport/urls.py
@@ -3,6 +3,6 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.show_route, name='bus_routes'),
     url(r'^(?P<id>\d+)$', views.show_route, name='show_route'),
-    url(r'^text-only/', views.show_route_textonly, name='bus_routes_textonly'),
     url(r'^text-only/(?P<id>\d+)$', views.show_route_textonly, name='show_route_textonly')
+    url(r'^text-only/', views.show_route_textonly, name='bus_routes_textonly'),
 )
